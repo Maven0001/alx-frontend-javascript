fb3470038d8b735efa48039f23453d80f2ca3dbd
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-11-05 21:11:09 +0100

    Printing Teachers and Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,3 +16,47 @@ const teacher1: Teacher = {
   location: "London",
   contract: false,
 };
+
+interface Directors {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return "${firstName[0]} . ${lastName}";
+};
+
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Class implementing the interface
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  // Constructor matches StudentConstructor interface
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  // Returns "Currently working"
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  // Returns the student's first name
+  displayName(): string {
+    return this.firstName;
+  }
+}
