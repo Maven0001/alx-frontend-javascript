f57e2457b3345fdae07675954748cf1d912db316
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-11-06 02:11:09 +0100

    Advance types part 1

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,115 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  // contract: boolean;
+  [propName: string]: any;
+}
+
+const teacher1: Teacher = {
+  firstName: "John",
+  lastName: "Doe",
+  fullTimeEmployee: false,
+  yearsOfExperience: 3,
+  location: "London",
+  contract: false,
+};
+
+/////////////////////TASK 02//////////////
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+/////////////////////TASK 03//////////////
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+/////////////////////TASK 04//////////////
+
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Class implementing the interface
+class StudentClass {
+  private firstName: string;
+  private lastName: string;
+
+  // Constructor matches StudentConstructor interface
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  // Returns "Currently working"
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  // Returns the student's first name
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+/////////////////////TASK 05//////////////
+
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+  getCoffeeBreak(): string {
+    return "Getting a Coffee drink";
+  }
+  getToWork(): string {
+    return "Getting a coffee break";
+  }
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+  workDirectorTasks(): string {
+    return "Getting to work";
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  }
+  return new Director();
+}
